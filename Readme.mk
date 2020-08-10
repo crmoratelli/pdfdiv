c87cf991ce51021e6455009bff928bce2d9ada30
Carlos Moratelli <crmoratelli@gmail.com>
2020-08-10 09:45:10 -0300

    Add Readme file

---
+++ Readme.mk
@@ -0,0 +1,9 @@
+Split a pdf file in multiple files.
+
+1. Install requiriments.
+	sudo pip3 install -r requiriments.txt
+
+2. Execute.
+	python3 pdfdiv.py <dir>
+
+It will read all pdfs in the directory and save their pages as a file's sequence in a new directory with the same file name. The pages will be numbered as 00001, 00002...
